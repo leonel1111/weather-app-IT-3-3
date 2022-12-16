26240d5131ca2ca49abaf040851204eace393202
leonel1111 <gubanleonelangelo@gmail.com>
2022-12-16 21:21:04 +0800

    the data in the location and forecast is now on the website page

--- public/js/app.js
+++ public/js/app.js
@@ -2,19 +2,27 @@ console.log('client side javascipt!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const  messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = '' 
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
 
+    messageOne.textContent = 'Loading...'
+    messageTwo .textContent = ''
+
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
-            console.log(data.error)
+            messageOne.textContent = data.error 
         } else {
-            console.log(data.location)
-            console.log(data.forecast)
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
         }
     }) 
 })
